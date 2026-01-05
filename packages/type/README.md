35b1dfc60eec06f957d9831f2a286dcd78f24acf
明兵 <18512863641@163.com>
2026-01-05 15:21:33 +0800

    init: 初始化项目

---
+++ packages/type/README.md
@@ -0,0 +1,5 @@
+## `@shuttle-data/type`
+
+#### 说明
+
+该部分是shuttle-data的类型定义包，提供数据模型的类型定义
---
+++ packages/type/src/dataModel.ts
@@ -0,0 +1,45 @@
+export namespace DataModel {
+  export interface BaseField<T extends string, E = undefined> {
+    type: T;
+    name: string;
+    apiName: string;
+    label?: string;
+    required?: boolean;
+
+    extra?: E
+  }
+
+  export interface BooleanField extends BaseField<'boolean'> {}
+  export interface StringField extends BaseField<'string', { maxLength?: number }> {}
+  export interface TextField extends BaseField<'text'> {}
+  export interface NumberField extends BaseField<'number', { autoIncrement?: boolean }> {}
+  export interface DoubleField extends BaseField<'double'> {}
+  export interface DateTimeField extends BaseField<'datetime'> {}
+  export interface DateField extends BaseField<'date'> {}
+  export interface EnumField extends BaseField<'enum', {
+    groupName: string
+    multiple?: boolean
+  }> {}
+  export interface LookupField extends BaseField<'lookup', {
+    modalName: string
+    multiple?: boolean
+  }> {}
+  export interface JsonField extends BaseField<'json'> {}
+
+  export interface FieldMap {
+    boolean: BooleanField
+    string: StringField
+    text: TextField
+    number: NumberField
+    double: DoubleField
+    datetime: DateTimeField
+    date: DateField
+    enum: EnumField
+    lookup: LookupField
+    json: JsonField
+  }
+
+  export type Field = FieldMap[keyof FieldMap]
+
+  export type FieldType = Field['type']
+}
